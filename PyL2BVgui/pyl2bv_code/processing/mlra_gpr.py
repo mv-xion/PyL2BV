500a7a20eefd6fe05dc892efe9af0402ae7bc14a
mvixion <151645543+mv-xion@users.noreply.github.com>
2024-11-01 13:00:20 +0100

    Code cleanup
    Deleting unused functions

--- PyL2BVgui/pyl2bv_code/processing/mlra_gpr.py
+++ PyL2BVgui/pyl2bv_code/processing/mlra_gpr.py
@@ -30,49 +30,6 @@ class MLRA_GPR(MLRA_Methods):
         self.Linv_pre_calc_GREEN = bio_model["Linv_pre_calc_GREEN"]
         self.hyp_sig_unc_GREEN = bio_model["hyp_sig_unc_GREEN"]
 
-    def GPR_mapping_pixel(self, pixel_spectra) -> tuple:
-        try:
-            im_norm_ell2D = pixel_spectra
-            im_norm_ell2D_hypell = im_norm_ell2D * self.hyp_ell_GREEN
-
-            im_norm_ell2D = im_norm_ell2D.reshape(-1, 1)
-            im_norm_ell2D_hypell = im_norm_ell2D_hypell.reshape(-1, 1)
-
-            PtTPt = np.matmul(
-                np.transpose(im_norm_ell2D_hypell), im_norm_ell2D
-            ).ravel() * (-0.5)
-            PtTDX = (
-                np.matmul(self.X_train_GREEN, im_norm_ell2D_hypell)
-                .ravel()
-                .flatten()
-            )
-
-            arg1 = np.exp(PtTPt) * self.hyp_sig_GREEN
-            k_star = np.exp(PtTDX - (self.XDX_pre_calc_GREEN.ravel() * 0.5))
-
-            mean_pred = (
-                np.dot(k_star.ravel(), self.alpha_coefficients_GREEN.ravel())
-                * arg1
-            ) + self.mean_model_GREEN
-            filterDown = np.greater(mean_pred, 0).astype(int)
-            mean_pred = mean_pred * filterDown
-
-            k_star_uncert = (
-                np.exp(PtTDX - (self.XDX_pre_calc_GREEN.ravel() * 0.5)) * arg1
-            )
-            Vvector = np.matmul(
-                self.Linv_pre_calc_GREEN, k_star_uncert.reshape(-1, 1)
-            ).ravel()
-
-            Variance = math.sqrt(
-                abs(self.hyp_sig_unc_GREEN - np.dot(Vvector, Vvector))
-            )
-
-            return mean_pred.item(), Variance
-        except Exception as e:
-            logging.error(f"Error in GPR_mapping_pixel: {e}")
-            raise
-
     def process_pixel_batch(self, batch: np.ndarray) -> tuple:
         im_norm_ell2D = batch
         im_norm_ell2D_hypell = im_norm_ell2D * self.hyp_ell_GREEN
